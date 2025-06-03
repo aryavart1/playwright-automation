47273b2d8a49a08df93a582e275e90a95be2653a
Aryavart <mailaryavart@gmail.com>
2025-06-04 00:25:42 +0530

    updated

---
+++ contents/LoginPageContents.ts
@@ -0,0 +1,8 @@
+const LoginContent = {
+  LoginPageURL: "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login",
+  PageTitle: "OrangeHRM",
+  LoginErrorMessage: "Invalid credentials",
+  LoggedInUserName: "Welcome Admin"
+
+}
+export default LoginContent;
