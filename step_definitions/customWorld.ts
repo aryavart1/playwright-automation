a5304799a1768e3bf7244cd4db1182ff720957c8
Aryavart <mailaryavart@gmail.com>
2025-06-09 23:22:34 +0530

    added customWorld.ts file

--- step_definitions/customWorld.ts
+++ step_definitions/customWorld.ts
@@ -1,1 +1,29 @@
-import {setWorldConstructor} from '@cucumber/cucumber'
+import { setWorldConstructor } from '@cucumber/cucumber';
+import { BddWorld, BddWorldOptions } from 'playwright-bdd';  // npm install playwright-bdd@6.6.0 --save-dev
+
+import { LoginPage } from '../pages/LoginPage';
+import { DashboardPage } from '../pages/Dashboard.page';
+
+export class CustomWorld extends BddWorld {
+    constructor(public options: BddWorldOptions) {
+        super(options);
+    }
+    
+    async init() {
+         // This method helps to setup beforeEach function for your scripts
+    }
+
+    async destroy() {
+         // This method helps to setup afterEach function for your scripts
+    }
+
+    // following methods shows how to add your project specific Pages to your Basetest and make it available for stepdef
+
+    get loginPage(): LoginPage {
+      return new LoginPage(this.page);
+    }
+
+    get dashboardPage(): DashboardPage {
+      return new DashboardPage(this.page);
+    } 
+}
